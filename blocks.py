c50f985c97785c5165003f1f422d32d47d8d997d
mattdodge <matt@n.io>
2015-04-01 14:41:11 -0700

    Adds version to example block

--- blocks.py
+++ blocks.py
@@ -2,8 +2,9 @@ from .generators.counter import CounterGenerator
 from .triggers.interval import IntervalTrigger
 from nio.common.block.base import Block
 from nio.common.discovery import Discoverable, DiscoverableType
+from nio.metadata.properties import VersionProperty
 
 
 @Discoverable(DiscoverableType.block)
 class CounterIntervalSimulator(CounterGenerator, IntervalTrigger, Block):
-    pass
+    version = VersionProperty('1.0.0')
